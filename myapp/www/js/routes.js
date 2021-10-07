f1185118b34d54309a5d6f954f2b54fd0e3ddc1d
mandstoni <62080291+mandstoni@users.noreply.github.com>
2021-10-07 20:43:24 -0300

    pt 2 tcc

---
+++ myapp/www/js/routes.js
@@ -0,0 +1,45 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('page1', {
+    url: '/login',
+    templateUrl: 'templates/page1.html',
+    controller: 'page1Ctrl'
+  })
+
+  .state('page2', {
+    url: '/cadastro1',
+    templateUrl: 'templates/page2.html',
+    controller: 'page2Ctrl'
+  })
+
+  .state('page3', {
+    url: '/cadastro2',
+    templateUrl: 'templates/page3.html',
+    controller: 'page3Ctrl'
+  })
+
+  .state('page4', {
+    url: '/login',
+    templateUrl: 'templates/page4.html',
+    controller: 'page4Ctrl'
+  })
+
+  .state('page5', {
+    url: '/cadastroSucesso',
+    templateUrl: 'templates/page5.html',
+    controller: 'page5Ctrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+
+});
